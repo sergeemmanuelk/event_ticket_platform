1a499359674db5decd9b39ae1f6ad0b47dc291ba
sergeemmanuelk <serge_em@outlook.fr>
2025-05-18 21:22:35 +0000

    create user repository interface

---
+++ ticket-api/src/main/java/dev/skonan/tickets/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package dev.skonan.tickets.repositories;
+
+import dev.skonan.tickets.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
