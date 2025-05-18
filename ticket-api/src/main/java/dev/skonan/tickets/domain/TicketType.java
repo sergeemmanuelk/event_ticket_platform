d679a9d308995e378f110d015d51a5fa499dab73
sergeemmanuelk <serge_em@outlook.fr>
2025-05-18 21:43:08 +0000

    add new field to TicketType entity

--- ticket-api/src/main/java/dev/skonan/tickets/domain/TicketType.java
+++ ticket-api/src/main/java/dev/skonan/tickets/domain/TicketType.java
@@ -31,6 +31,9 @@ public class TicketType {
     @Column(name = "price", nullable = false)
     private Double price;
 
+    @Column(name = "description")
+    private String description;
+
     @Column(name = "total_available")
     private Integer totalAvailable;
 
