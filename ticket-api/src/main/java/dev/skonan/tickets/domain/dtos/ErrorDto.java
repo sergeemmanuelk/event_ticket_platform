8aee876c4124a7b0da59c868f956757a318c3386
sergeemmanuelk <serge_em@outlook.fr>
2025-05-19 13:01:01 +0000

    implement global exception handler

---
+++ ticket-api/src/main/java/dev/skonan/tickets/domain/dtos/ErrorDto.java
@@ -0,0 +1,13 @@
+package dev.skonan.tickets.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDto {
+
+    private String error;
+}
