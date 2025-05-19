9bac991a99a98f2e3fb13a134e1895193bd1992c
sergeemmanuelk <serge_em@outlook.fr>
2025-05-19 12:12:32 +0000

    implement createEvent method of event controller

---
+++ ticket-api/src/main/java/dev/skonan/tickets/controllers/EventController.java
@@ -0,0 +1,43 @@
+package dev.skonan.tickets.controllers;
+
+import dev.skonan.tickets.domain.CreateEventRequest;
+import dev.skonan.tickets.domain.dtos.CreateEventRequestDto;
+import dev.skonan.tickets.domain.dtos.CreateEventResponseDto;
+import dev.skonan.tickets.domain.entities.Event;
+import dev.skonan.tickets.domain.mappers.EventMapper;
+import dev.skonan.tickets.services.EventService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(params = "events")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventMapper eventMapper;
+    private final EventService eventService;
+
+    @PostMapping
+    public ResponseEntity<CreateEventResponseDto> createEvent(
+            @AuthenticationPrincipal Jwt jwt,
+            @Valid @RequestBody CreateEventRequestDto createEventRequestDto) {
+
+        CreateEventRequest createEventRequest = eventMapper.fromDto(createEventRequestDto);
+        UUID userId = UUID.fromString(jwt.getSubject());
+
+        Event createdEvent = eventService.createEvent(userId, createEventRequest);
+        CreateEventResponseDto createEventResponseDto = eventMapper.toDto(createdEvent);
+
+        return new ResponseEntity<>(createEventResponseDto, HttpStatus.CREATED);
+    }
+}
