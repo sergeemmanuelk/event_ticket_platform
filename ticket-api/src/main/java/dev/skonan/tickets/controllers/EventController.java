5d856590a44f28d7c51ed6f444754304d9d49f0b
sergeemmanuelk <serge_em@outlook.fr>
2025-06-11 13:35:42 +0000

    fix request mapping argument

--- ticket-api/src/main/java/dev/skonan/tickets/controllers/EventController.java
+++ ticket-api/src/main/java/dev/skonan/tickets/controllers/EventController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(params = "events")
+@RequestMapping(path = "events")
 @RequiredArgsConstructor
 public class EventController {
 
